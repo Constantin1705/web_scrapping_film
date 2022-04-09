b899924092a03c2de1119e52485fd4a195c9da4c
Constantin1705 <56501940+Constantin1705@users.noreply.github.com>
2022-04-09 12:18:54 +0300

    movies

---
+++ web_scrapping_film/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for web_scrapping_film project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web_scrapping_film.settings')
+
+application = get_wsgi_application()
